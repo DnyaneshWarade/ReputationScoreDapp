267c9ab957e878b7c0785bdd851b51ea14f81ad6
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-02-11 19:54:39 +0530

    graph api added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	uri: "https://api.studio.thegraph.com/query/41978/aave/v0.0.1",
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>
 );
---
+++ src/services/graph-api-data.js
@@ -0,0 +1,123 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_BORROWS = gql`
+	query GetBorrows($account: String) {
+		borrows(where: { user: $account }) {
+			id
+			reserve
+			user
+			amount
+			borrowRate
+		}
+	}
+`;
+const GET_DEPOSITS = gql`
+	query GetDeposits($account: String) {
+		deposits(where: { user: $account }) {
+			id
+			reserve
+			user
+			amount
+		}
+	}
+`;
+const GET_FLASH_LOANS = gql`
+	query GetFlashLoans($account: String) {
+		flashLoans(where: { initiator: $account }) {
+			id
+			target
+			initiator
+			asset
+			amount
+			premium
+		}
+	}
+`;
+
+const GET_LIQUIDATION_CALLS = gql`
+	query GetLiquidationCalls($account: String) {
+		liquidationCalls(where: { user: $account }) {
+			id
+			collateralAsset
+			debtAsset
+			user
+			debtToCover
+			liquidatedCollateralAmount
+			liquidator
+		}
+	}
+`;
+const GET_REPAYS = gql`
+	query GetRepays($account: String) {
+		repays(where: { user: $account }) {
+			id
+			reserve
+			user
+			repayer
+			amount
+		}
+	}
+`;
+const GET_SWAPS = gql`
+	query GetSwaps($account: String) {
+		swaps(where: { user: $account }) {
+			id
+			reserve
+			user
+			rateMode
+		}
+	}
+`;
+const GET_WITHDRAWS = gql`
+	query GetWithdraws($account: String) {
+		withdraws(where: { user: $account }) {
+			id
+			reserve
+			user
+			to
+			amount
+		}
+	}
+`;
+export function GetBorrows(account) {
+	const { loading, error, data } = useQuery(GET_BORROWS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetDeposits(account) {
+	const { loading, error, data } = useQuery(GET_DEPOSITS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetFlashLoans(account) {
+	const { loading, error, data } = useQuery(GET_FLASH_LOANS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetLiquidationCalls(account) {
+	const { loading, error, data } = useQuery(GET_LIQUIDATION_CALLS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetRepays(account) {
+	const { loading, error, data } = useQuery(GET_REPAYS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetSwaps(account) {
+	const { loading, error, data } = useQuery(GET_SWAPS, {
+		variables: { account },
+	});
+	console.log(data);
+}
+export function GetWithdraws(account) {
+	const { loading, error, data } = useQuery(GET_WITHDRAWS, {
+		variables: { account },
+	});
+	console.log(data);
+}
