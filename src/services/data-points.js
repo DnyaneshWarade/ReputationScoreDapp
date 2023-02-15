13d57bd49e7dd913a87253765e84af7e739115ff
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-02-15 19:46:40 +0530

    fixed graph api call issue

--- src/services/data-points.js
+++ src/services/data-points.js
@@ -1,325 +1,341 @@
 import axiosHttpService from "./axioscall";
 import {
-    ethTransactions,
-    ethTokenTransactions,
+	ethTransactions,
+	ethTokenTransactions,
 } from "./blockchainTransactionDataOptions";
 import { ethers } from "ethers";
 import { walletTokenBalances } from "./covalentDataOptions";
 import {
-    fetchNFTs,
-    fetchNFTCollectionDetails,
-    fetchNFTsByCollection,
-    getTransfersByNFT,
-    verifyNFTsOwner
+	fetchNFTs,
+	fetchNFTCollectionDetails,
+	fetchNFTsByCollection,
+	getTransfersByNFT,
+	verifyNFTsOwner,
 } from "./quicknode-nftdata";
 import { getBalance } from "./kucoin-api-calls";
 import { getBalances } from "./coindcx-api-calls";
+import {
+	getBorrows,
+	getDeposits,
+	getFlashLoans,
+	getLiquidationCalls,
+	getRepays,
+	getSwaps,
+	getWithdraws,
+} from "./graph-api-data";
 
 const topToken = [
-    "BTC",
-    "aWBTC",
-    "WBTC",
-    "ETH",
-    "WETH",
-    "aETH",
-    "aWETH",
-    "sETH",
-    "aUniLINKETH",
-    "aUniETH",
-    "USDT",
-    "aUSDT",
-    "BNB",
-    "USDC",
-    "aUSDC",
-    "aUniUSDC",
-    "BUSD",
-    "aBUSD",
-    "XRP",
-    "ADA",
-    "MATIC",
-    "DOT",
-    "DAI",
-    "aDAI",
-    "aUniDAI",
+	"BTC",
+	"aWBTC",
+	"WBTC",
+	"ETH",
+	"WETH",
+	"aETH",
+	"aWETH",
+	"sETH",
+	"aUniLINKETH",
+	"aUniETH",
+	"USDT",
+	"aUSDT",
+	"BNB",
+	"USDC",
+	"aUSDC",
+	"aUniUSDC",
+	"BUSD",
+	"aBUSD",
+	"XRP",
+	"ADA",
+	"MATIC",
+	"DOT",
+	"DAI",
+	"aDAI",
+	"aUniDAI",
 ];
 const memTokens = ["DOGE", "SHIB"];
 
 function borrowerFilter(tx) {
-    return tx.functionName.toLowerCase().includes("borrow");
+	return tx.functionName.toLowerCase().includes("borrow");
 }
 
 function nonBorrowerFilter(tx) {
-    return tx.functionName.toLowerCase().indexOf("borrow") === -1;
+	return tx.functionName.toLowerCase().indexOf("borrow") === -1;
 }
 
 function liquidationFilter(tx) {
-    return tx.functionName.toLowerCase().includes("liquidation");
+	return tx.functionName.toLowerCase().includes("liquidation");
 }
 
 function getDayDiff(epochTimeStamp) {
-    if (epochTimeStamp) {
-        let timeDiff = Date.now() / 1000 - epochTimeStamp;
-        return timeDiff / (24 * 60 * 60);
-    }
+	if (epochTimeStamp) {
+		let timeDiff = Date.now() / 1000 - epochTimeStamp;
+		return timeDiff / (24 * 60 * 60);
+	}
 }
 
 async function getAllTransactions(address) {
-    try {
-        if (!address) {
-            return;
-        }
-        let transactionDetails = await axiosHttpService(ethTransactions(address));
+	try {
+		if (!address) {
+			return;
+		}
+		let transactionDetails = await axiosHttpService(ethTransactions(address));
 
-        if (
-            transactionDetails &&
-            transactionDetails.res &&
-            transactionDetails.res.result.length > 0
-        ) {
-            //console.log(transactionDetails.res.result);
-            return transactionDetails.res.result;
-        }
-    } catch (error) {
-        console.log(error);
-    }
+		if (
+			transactionDetails &&
+			transactionDetails.res &&
+			transactionDetails.res.result.length > 0
+		) {
+			//console.log(transactionDetails.res.result);
+			return transactionDetails.res.result;
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function getFirstTransactionDateDiff(transactions) {
-    try {
-        if (!transactions) {
-            return;
-        }
+	try {
+		if (!transactions) {
+			return;
+		}
 
-        // First transaction on the chain from this address
-        let timeDiffInDays = getDayDiff(transactions[0].timeStamp);
-        console.log("First transaction on the wallet adddress: ", timeDiffInDays);
-        return timeDiffInDays;
-    } catch (error) {
-        console.log(error);
-    }
+		// First transaction on the chain from this address
+		let timeDiffInDays = getDayDiff(transactions[0].timeStamp);
+		console.log("First transaction on the wallet adddress: ", timeDiffInDays);
+		return timeDiffInDays;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function getFirstBorrowDateDiff(transactions) {
-    try {
-        if (!transactions) {
-            return;
-        }
+	try {
+		if (!transactions) {
+			return;
+		}
 
-        // Borrowed amount
-        const borrowTrxs = transactions.filter(borrowerFilter);
-        // First borrowing on the chain from this address
-        let timeDiffInDays = getDayDiff(borrowTrxs[0].timeStamp);
-        console.log("First borrowing on the wallet adddress: ", timeDiffInDays);
-        return timeDiffInDays;
-    } catch (error) {
-        console.log(error);
-    }
+		// Borrowed amount
+		const borrowTrxs = transactions.filter(borrowerFilter);
+		// First borrowing on the chain from this address
+		let timeDiffInDays = getDayDiff(borrowTrxs[0].timeStamp);
+		console.log("First borrowing on the wallet adddress: ", timeDiffInDays);
+		return timeDiffInDays;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function getAllTokenTransactions(address) {
-    try {
-        if (!address) {
-            return;
-        }
-        let transactionDetails = await axiosHttpService(
-            ethTokenTransactions(address)
-        );
+	try {
+		if (!address) {
+			return;
+		}
+		let transactionDetails = await axiosHttpService(
+			ethTokenTransactions(address)
+		);
 
-        if (
-            transactionDetails &&
-            transactionDetails.res &&
-            transactionDetails.res.result.length > 0
-        ) {
-            console.log(transactionDetails.res.result);
-            return transactionDetails.res.result;
-        }
-    } catch (error) {
-        console.log(error);
-    }
+		if (
+			transactionDetails &&
+			transactionDetails.res &&
+			transactionDetails.res.result.length > 0
+		) {
+			console.log(transactionDetails.res.result);
+			return transactionDetails.res.result;
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function getAvgAssetBrought(allTrx, tokenTrx, address) {
-    try {
-        if (!allTrx || !tokenTrx || !address) {
-            return;
-        }
+	try {
+		if (!allTrx || !tokenTrx || !address) {
+			return;
+		}
 
-        // filter all transaction for non borrowings
-        const nonBorrowTrxs = allTrx.filter(nonBorrowerFilter);
-        let counter = 0,
-            amount = 0;
-        nonBorrowTrxs.forEach((tx) => {
-            let transaction = tokenTrx.find(
-                (tokenTx) =>
-                    tokenTx.hash.toLowerCase() === tx.hash.toLowerCase() &&
-                    tokenTx.to.toLowerCase() === address.toLowerCase()
-            );
-            if (transaction) {
-                let txAmount = ethers.utils.formatUnits(
-                    transaction.value,
-                    transaction.tokenDecimal
-                );
-                amount += +txAmount;
-                ++counter;
-            }
-        });
+		// filter all transaction for non borrowings
+		const nonBorrowTrxs = allTrx.filter(nonBorrowerFilter);
+		let counter = 0,
+			amount = 0;
+		nonBorrowTrxs.forEach((tx) => {
+			let transaction = tokenTrx.find(
+				(tokenTx) =>
+					tokenTx.hash.toLowerCase() === tx.hash.toLowerCase() &&
+					tokenTx.to.toLowerCase() === address.toLowerCase()
+			);
+			if (transaction) {
+				let txAmount = ethers.utils.formatUnits(
+					transaction.value,
+					transaction.tokenDecimal
+				);
+				amount += +txAmount;
+				++counter;
+			}
+		});
 
-        if (counter > 0) {
-            let avgAmount = amount / counter;
-            console.log("Avarage asset transaction amount = ", avgAmount);
-            return avgAmount;
-        }
-    } catch (error) {
-        console.log(error);
-    }
+		if (counter > 0) {
+			let avgAmount = amount / counter;
+			console.log("Avarage asset transaction amount = ", avgAmount);
+			return avgAmount;
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function getLiquidationCount(transactions) {
-    try {
-        if (!transactions) {
-            return;
-        }
+	try {
+		if (!transactions) {
+			return;
+		}
 
-        // Borrowed amount
-        const liquidationTrxs = transactions.filter(liquidationFilter);
-        // First borrowing on the chain from this address
-        console.log("Liquidation count: ", liquidationTrxs.length);
-        return liquidationTrxs.length;
-    } catch (error) {
-        console.log(error);
-    }
+		// Borrowed amount
+		const liquidationTrxs = transactions.filter(liquidationFilter);
+		// First borrowing on the chain from this address
+		console.log("Liquidation count: ", liquidationTrxs.length);
+		return liquidationTrxs.length;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function getAllTokenBalances(address) {
-    try {
-        if (!address) {
-            return;
-        }
-        let transactionDetails = await axiosHttpService(
-            walletTokenBalances(address)
-        );
-        if (
-            transactionDetails &&
-            transactionDetails.res &&
-            transactionDetails.res.data.items.length > 0
-        ) {
-            let goodTokenAmt = 0;
-            let totalTokenAmt = 0;
-            let memeTokenCount = 0;
-            console.log(transactionDetails.res.data.items);
-            transactionDetails.res.data.items.forEach((trx) => {
-                let found = topToken.find(
-                    (symbol) =>
-                        symbol.toLowerCase() === trx.contract_ticker_symbol?.toLowerCase()
-                );
-                if (trx.balance && trx.balance !== "0" && trx.contract_decimals !== 0) {
-                    let txAmount = ethers.utils.formatUnits(
-                        trx.balance,
-                        trx.contract_decimals
-                    );
-                    totalTokenAmt += +txAmount;
-                    if (found) {
-                        goodTokenAmt += +txAmount;
-                    }
-                }
+	try {
+		if (!address) {
+			return;
+		}
+		let transactionDetails = await axiosHttpService(
+			walletTokenBalances(address)
+		);
+		if (
+			transactionDetails &&
+			transactionDetails.res &&
+			transactionDetails.res.data.items.length > 0
+		) {
+			let goodTokenAmt = 0;
+			let totalTokenAmt = 0;
+			let memeTokenCount = 0;
+			console.log(transactionDetails.res.data.items);
+			transactionDetails.res.data.items.forEach((trx) => {
+				let found = topToken.find(
+					(symbol) =>
+						symbol.toLowerCase() === trx.contract_ticker_symbol?.toLowerCase()
+				);
+				if (trx.balance && trx.balance !== "0" && trx.contract_decimals !== 0) {
+					let txAmount = ethers.utils.formatUnits(
+						trx.balance,
+						trx.contract_decimals
+					);
+					totalTokenAmt += +txAmount;
+					if (found) {
+						goodTokenAmt += +txAmount;
+					}
+				}
 
-                // check for meme token
+				// check for meme token
 
-                let memFound = memTokens.find(
-                    (symbol) =>
-                        symbol.toLowerCase() === trx.contract_ticker_symbol?.toLowerCase()
-                );
-                if (memFound) {
-                    ++memeTokenCount;
-                }
-            });
+				let memFound = memTokens.find(
+					(symbol) =>
+						symbol.toLowerCase() === trx.contract_ticker_symbol?.toLowerCase()
+				);
+				if (memFound) {
+					++memeTokenCount;
+				}
+			});
 
-            if (totalTokenAmt > 0) {
-                let goodTokenBal = (goodTokenAmt * 100) / totalTokenAmt;
-                return { goodTokenBal, memeTokenCount };
-            }
-        }
-    } catch (error) {
-        console.log(error);
-    }
+			if (totalTokenAmt > 0) {
+				let goodTokenBal = (goodTokenAmt * 100) / totalTokenAmt;
+				return { goodTokenBal, memeTokenCount };
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 export async function getCreditScore(address) {
-    // Fetching data from Quick Node
-    await fetchNFTs();
-    await fetchNFTCollectionDetails();
-    await fetchNFTsByCollection();
-    await getTransfersByNFT();
-    await verifyNFTsOwner();
+	// Fetching data from Quick Node
+	await fetchNFTs();
+	await fetchNFTCollectionDetails();
+	await fetchNFTsByCollection();
+	await getTransfersByNFT();
+	await verifyNFTsOwner();
 
-    // Fetching Graph data
+	// Fetching Graph data
+	await getBorrows("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getDeposits("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getFlashLoans("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getLiquidationCalls("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getRepays("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getSwaps("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
+	await getWithdraws("0xf1fa3b44c153f7988ff5de730b138eec476a7533");
 
-    // Fetching KuCoin Data
-    await getBalance();
-    // fetching CoinDCX data
-    await getBalances();
+	// Fetching KuCoin Data
+	await getBalance();
+	// fetching CoinDCX data
+	await getBalances();
 
-    const trx = await getAllTransactions(address);
-    let ageInDays = getFirstTransactionDateDiff(trx);
-    let avgAgeScore = 0;
-    if (ageInDays < 180) {
-        avgAgeScore = 0.15 * 300;
-    } else if (ageInDays >= 180 && ageInDays <= 365) {
-        avgAgeScore = 0.15 * 650;
-    } else {
-        avgAgeScore = 0.15 * 900;
-    }
+	const trx = await getAllTransactions(address);
+	let ageInDays = getFirstTransactionDateDiff(trx);
+	let avgAgeScore = 0;
+	if (ageInDays < 180) {
+		avgAgeScore = 0.15 * 300;
+	} else if (ageInDays >= 180 && ageInDays <= 365) {
+		avgAgeScore = 0.15 * 650;
+	} else {
+		avgAgeScore = 0.15 * 900;
+	}
 
-    let firstBorrowInDays = getFirstBorrowDateDiff(trx);
-    let firstBorrowAgeScore = 0;
-    if (firstBorrowInDays < 180) {
-        firstBorrowAgeScore = 0.15 * 300;
-    } else if (firstBorrowInDays >= 180 && firstBorrowInDays <= 365) {
-        firstBorrowAgeScore = 0.15 * 650;
-    } else {
-        firstBorrowAgeScore = 0.15 * 900;
-    }
+	let firstBorrowInDays = getFirstBorrowDateDiff(trx);
+	let firstBorrowAgeScore = 0;
+	if (firstBorrowInDays < 180) {
+		firstBorrowAgeScore = 0.15 * 300;
+	} else if (firstBorrowInDays >= 180 && firstBorrowInDays <= 365) {
+		firstBorrowAgeScore = 0.15 * 650;
+	} else {
+		firstBorrowAgeScore = 0.15 * 900;
+	}
 
-    const tokenTrx = await getAllTokenTransactions(address);
-    let assetAmt = getAvgAssetBrought(trx, tokenTrx, address);
-    let assetAmtScore = 0;
-    if (assetAmt < 1000) {
-        assetAmtScore = 0.15 * 300;
-    } else if (assetAmt >= 1000 && assetAmt <= 10000) {
-        assetAmtScore = 0.15 * 650;
-    } else {
-        assetAmtScore = 0.15 * 900;
-    }
+	const tokenTrx = await getAllTokenTransactions(address);
+	let assetAmt = getAvgAssetBrought(trx, tokenTrx, address);
+	let assetAmtScore = 0;
+	if (assetAmt < 1000) {
+		assetAmtScore = 0.15 * 300;
+	} else if (assetAmt >= 1000 && assetAmt <= 10000) {
+		assetAmtScore = 0.15 * 650;
+	} else {
+		assetAmtScore = 0.15 * 900;
+	}
 
-    let { goodTokenBal, memeTokenCount } = await getAllTokenBalances(address);
-    let goodTokenScore = 0;
-    let memeTokenScore = 0;
-    if (goodTokenBal < 50) {
-        goodTokenScore = 0.15 * 300;
-    } else if (goodTokenBal >= 50 && goodTokenBal <= 70) {
-        goodTokenScore = 0.15 * 650;
-    } else {
-        goodTokenScore = 0.15 * 900;
-    }
-    if (memeTokenCount > 0) {
-        memeTokenScore = 0.3 * 300;
-    } else {
-        memeTokenScore = 0.3 * 900;
-    }
+	let { goodTokenBal, memeTokenCount } = await getAllTokenBalances(address);
+	let goodTokenScore = 0;
+	let memeTokenScore = 0;
+	if (goodTokenBal < 50) {
+		goodTokenScore = 0.15 * 300;
+	} else if (goodTokenBal >= 50 && goodTokenBal <= 70) {
+		goodTokenScore = 0.15 * 650;
+	} else {
+		goodTokenScore = 0.15 * 900;
+	}
+	if (memeTokenCount > 0) {
+		memeTokenScore = 0.3 * 300;
+	} else {
+		memeTokenScore = 0.3 * 900;
+	}
 
-    let liquidationNo = getLiquidationCount(trx);
-    let liquidationScore = 0;
-    if (liquidationNo > 0) {
-        liquidationScore = 0.3 * 300;
-    } else {
-        liquidationScore = 0.3 * 900;
-    }
+	let liquidationNo = getLiquidationCount(trx);
+	let liquidationScore = 0;
+	if (liquidationNo > 0) {
+		liquidationScore = 0.3 * 300;
+	} else {
+		liquidationScore = 0.3 * 900;
+	}
 
-    return (
-        avgAgeScore +
-        firstBorrowAgeScore +
-        assetAmtScore +
-        goodTokenScore +
-        memeTokenScore +
-        liquidationScore
-    );
+	return (
+		avgAgeScore +
+		firstBorrowAgeScore +
+		assetAmtScore +
+		goodTokenScore +
+		memeTokenScore +
+		liquidationScore
+	);
 }
--- src/services/graph-api-data.js
+++ src/services/graph-api-data.js
@@ -1,129 +1,170 @@
-import { useQuery, gql } from "@apollo/client";
+import axios from "axios";
 
-const GET_BORROWS = gql`
-	query GetBorrows($account: String) {
-		borrows(where: { user: $account }) {
-			id
-			reserve
-			user
-			amount
-			borrowRate
-		}
-	}
-`;
-const GET_DEPOSITS = gql`
-	query GetDeposits($account: String) {
-		deposits(where: { user: $account }) {
-			id
-			reserve
-			user
-			amount
-		}
-	}
-`;
-const GET_FLASH_LOANS = gql`
-	query GetFlashLoans($account: String) {
-		flashLoans(where: { initiator: $account }) {
-			id
-			target
-			initiator
-			asset
-			amount
-			premium
-		}
-	}
-`;
+const URL = "https://api.studio.thegraph.com/query/41978/aave/v0.0.1";
 
-const GET_LIQUIDATION_CALLS = gql`
-	query GetLiquidationCalls($account: String) {
-		liquidationCalls(where: { user: $account }) {
-			id
-			collateralAsset
-			debtAsset
-			user
-			debtToCover
-			liquidatedCollateralAmount
-			liquidator
+export async function getBorrows(account) {
+	const query = `
+		{
+			borrows(where: {user: "${account}"}) {
+				id
+				reserve
+				user
+				amount
+				borrowRate
+			}
 		}
-	}
-`;
-const GET_REPAYS = gql`
-	query GetRepays($account: String) {
-		repays(where: { user: $account }) {
-			id
-			reserve
-			user
-			repayer
-			amount
-		}
-	}
-`;
-const GET_SWAPS = gql`
-	query GetSwaps($account: String) {
-		swaps(where: { user: $account }) {
-			id
-			reserve
-			user
-			rateMode
-		}
-	}
-`;
-const GET_WITHDRAWS = gql`
-	query GetWithdraws($account: String) {
-		withdraws(where: { user: $account }) {
-			id
-			reserve
-			user
-			to
-			amount
-		}
-	}
-`;
-export function GetBorrows(account) {
+	`;
+
 	console.log("================== Graph Data of Borrowing ==================");
-	const { loading, error, data } = useQuery(GET_BORROWS, {
-		variables: { account },
-	});
-	console.log(data);
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetDeposits(account) {
+
+export async function getDeposits(account) {
+	const query = `
+		{
+			deposits(where: {user: "${account}"}) {
+				id
+				reserve
+				user
+				amount
+			}
+		}
+	`;
+
 	console.log("================== Graph Data of Deposits ==================");
-	const { loading, error, data } = useQuery(GET_DEPOSITS, {
-		variables: { account },
-	});
-	console.log(data);
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetFlashLoans(account) {
-	console.log("================== Graph Data of Flash Loans ==================");
-	const { loading, error, data } = useQuery(GET_FLASH_LOANS, {
-		variables: { account },
-	});
-	console.log(data);
+
+export async function getFlashLoans(account) {
+	const query = `
+		{
+			flashLoans(where: {initiator: "${account}"}) {
+				id
+				target
+				initiator
+				asset
+				amount
+				premium
+			}
+		}
+	`;
+
+	console.log(
+		"================== Graph Data of Flash Loans =================="
+	);
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetLiquidationCalls(account) {
-	console.log("================== Graph Data of Liquidation ==================");
-	const { loading, error, data } = useQuery(GET_LIQUIDATION_CALLS, {
-		variables: { account },
-	});
-	console.log(data);
+
+export async function getLiquidationCalls(account) {
+	const query = `
+		{
+			liquidationCalls(where: {user: "${account}"}) {
+				id
+				collateralAsset
+				debtAsset
+				user
+				debtToCover
+				liquidatedCollateralAmount
+				liquidator
+			}
+		}
+	`;
+
+	console.log(
+		"================== Graph Data of Liquidation =================="
+	);
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetRepays(account) {
+
+export async function getRepays(account) {
+	const query = `
+		{
+			repays(where: {user: "${account}"}) {
+				id
+				reserve
+				user
+				repayer
+				amount
+			}
+		}
+	`;
+
 	console.log("================== Graph Data of Repayments ==================");
-	const { loading, error, data } = useQuery(GET_REPAYS, {
-		variables: { account },
-	});
-	console.log(data);
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetSwaps(account) {
-	const { loading, error, data } = useQuery(GET_SWAPS, {
-		variables: { account },
-	});
-	console.log(data);
+
+export async function getSwaps(account) {
+	const query = `
+		{
+			swaps(where: {user: "${account}"}) {
+				id
+				reserve
+				user
+				rateMode
+			}
+		}
+	`;
+
+	console.log("================== Graph Data of Swaps ==================");
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
-export function GetWithdraws(account) {
-	console.log("================== Graph Data of Withdrawals ==================");
-	const { loading, error, data } = useQuery(GET_WITHDRAWS, {
-		variables: { account },
-	});
-	console.log(data);
+
+export async function getWithdraws(account) {
+	const query = `
+		{
+			withdraws(where: {user: "${account}"}) {
+				id
+				reserve
+				user
+				to
+				amount
+			}
+		}
+	`;
+
+	console.log(
+		"================== Graph Data of Withdrawals =================="
+	);
+
+	try {
+		const result = await axios.post(URL, { query: query });
+		console.log(result.data.data);
+	} catch (error) {
+		console.log(error);
+	}
 }
